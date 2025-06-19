b7ea6844028dba8595b5a312675f5396305a47ea
Tausif Shaikh <shaikhtausif089@gmail.com>
2025-06-19 13:39:05 +0530

    add inngest-event-key

--- Backend/inngest/client.js
+++ Backend/inngest/client.js
@@ -3,13 +3,17 @@
 // For more : https://www.inngest.com/docs/getting-started/nodejs-quick-start?ref=docs-home&guide=express
 
 // Inngest receives a stream of events, and based on the event type, it automatically invokes the corresponding serverless function (or handler) that you’ve defined for that event.
+// Steps to do before deploying the inngest app is first to set the INNGEST_EVENT_KEY in the environment variable from https://app.inngest.com/env/production/onboarding/sync-app?nonVercel=true and then Enter the URL of your application's serve endpoint to register your functions with Inngest. 
+
 
 
 import { Inngest } from "inngest";
 
 
 // Create a client to send and receive events
-export const inngest = new Inngest({id : 'ticketing-system'});
+export const inngest = new Inngest({id : 'ticketing-system',
+    eventKey : process.env.INNGEST_EVENT_KEY , // Required in production,
+});
 
 
 
